04f7b69828b59c45dc1d5e4962333031fe3538ef
trishinanton <74793031+trishinanton@users.noreply.github.com>
2021-08-03 23:55:03 +0300

    03.08.21

---
+++ precache-manifest.4d380e015c7e5f8ef5aa5e478daba277.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80914993a0c39926092706d60aef4538",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8950f8caf67e19b37be",
+    "url": "/static/js/2.ebcfdeea.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/static/js/2.ebcfdeea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fea1b6a3f3ce8f566115",
+    "url": "/static/js/main.c865571b.chunk.js"
+  },
+  {
+    "revision": "b201489dcbcc9f77b2de",
+    "url": "/static/js/runtime-main.e19410a0.js"
+  }
+]);
